336f22e04948ecdc4e4ef3638bfd324d4613c227
Gabriel <emilio.nunez.arenas@gmail.com>
2023-09-21 12:43:12 -0500

    chore: proyect to proyects section

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -1,4 +1,6 @@
 import { CiFolderOn } from "react-icons/ci";
+import { FiGithub } from "react-icons/fi";
+import { PiArrowSquareOutBold } from "react-icons/pi";
 
 const Proyects = () => {
   return (
@@ -8,9 +10,95 @@ const Proyects = () => {
       </h1>
 
       <div className="w-full flex flex-wrap gap-12 p-[10px] md:justify-center">
-        <div className="h-[305px] w-[315px] bg-[#0c2247b3] p-8">
-          <div className="flex w-full">
-            <CiFolderOn fontSize={50} className="changer" />
+        <div className="h-[305px] w-[315px] bg-[#0c2247b3] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
+          <div className="flex w-full justify-between items-center ">
+            <CiFolderOn fontSize={50} color="#59e0cd" />
+            <div className="flex gap-4 items-center">
+              <FiGithub fontSize={20} className="changer" />
+              <PiArrowSquareOutBold fontSize={25} className="changer" />
+            </div>
+          </div>
+          <div className="grid mt-5 gap-5">
+            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
+              Get That Home
+            </h1>
+            <p className="text-[#8992af] ">
+              A Full Stack web application to buy and sell homes{" "}
+            </p>
+            <div className="flex gap-4 text-[#515667] text-sm">
+              <p>React</p>
+              <p>Emotion</p>
+              <p>Vite</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-[305px] w-[315px] bg-[#0c2247b3] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
+          <div className="flex w-full justify-between items-center ">
+            <CiFolderOn fontSize={50} color="#59e0cd" />
+            <div className="flex gap-4 items-center">
+              <FiGithub fontSize={20} className="changer" />
+              <PiArrowSquareOutBold fontSize={25} className="changer" />
+            </div>
+          </div>
+          <div className="grid mt-5 gap-5">
+            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
+              Get That Home
+            </h1>
+            <p className="text-[#8992af] ">
+              A Full Stack web application to buy and sell homes{" "}
+            </p>
+            <div className="flex gap-4 text-[#515667] text-sm">
+              <p>React</p>
+              <p>Emotion</p>
+              <p>Vite</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-[305px] w-[315px] bg-[#0c2247b3] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
+          <div className="flex w-full justify-between items-center ">
+            <CiFolderOn fontSize={50} color="#59e0cd" />
+            <div className="flex gap-4 items-center">
+              <FiGithub fontSize={20} className="changer" />
+              <PiArrowSquareOutBold fontSize={25} className="changer" />
+            </div>
+          </div>
+          <div className="grid mt-5 gap-5">
+            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
+              Get That Home
+            </h1>
+            <p className="text-[#8992af] ">
+              A Full Stack web application to buy and sell homes{" "}
+            </p>
+            <div className="flex gap-4 text-[#515667] text-sm">
+              <p>React</p>
+              <p>Emotion</p>
+              <p>Vite</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-[305px] w-[315px] bg-[#0c2247b3] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
+          <div className="flex w-full justify-between items-center ">
+            <CiFolderOn fontSize={50} color="#59e0cd" />
+            <div className="flex gap-4 items-center">
+              <FiGithub fontSize={20} className="changer" />
+              <PiArrowSquareOutBold fontSize={25} className="changer" />
+            </div>
+          </div>
+          <div className="grid mt-5 gap-5">
+            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
+              Get That Home
+            </h1>
+            <p className="text-[#8992af] ">
+              A Full Stack web application to buy and sell homes{" "}
+            </p>
+            <div className="flex gap-4 text-[#515667] text-sm">
+              <p>React</p>
+              <p>Emotion</p>
+              <p>Vite</p>
+            </div>
           </div>
         </div>
       </div>
