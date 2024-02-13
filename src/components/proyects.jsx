382d70762e9b9ea4d4203fece49a230e9b7bd34a
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-02-12 19:54:23 -0500

    chore: data file and map metod to rendering

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { CiFolderOn } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
 import { PiArrowSquareOutBold } from "react-icons/pi";
+import { proyects } from "@/data";
 
 const Proyects = () => {
   return (
@@ -14,183 +15,39 @@ const Proyects = () => {
       </h1>
 
       <div className="w-full flex flex-wrap gap-12 p-[10px] md:justify-center">
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/get-that-home-frontend "
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-              <Link
-                href="https://get-that-home-frontend.vercel.app/"
-                target="_blank"
-              >
-                <PiArrowSquareOutBold fontSize={25} className="changer" />
-              </Link>
+        {proyects.map((proyect) => {
+          return (
+            <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
+              <div className="flex w-full justify-between items-center ">
+                <CiFolderOn fontSize={50} color="#ff9b2f" />
+                <div className="flex gap-4 items-center">
+                  <Link href={proyect.linkGitHub} target="_blank">
+                    <FiGithub fontSize={20} className={`changer`} />
+                  </Link>
+                  <Link href={proyect.LinkDeployed} target="_blank">
+                    <PiArrowSquareOutBold
+                      fontSize={25}
+                      className={`changer ${
+                        proyect.LinkDeployed == "/" ? "hidden" : true
+                      }`}
+                    />
+                  </Link>
+                </div>
+              </div>
+              <div className="grid mt-5 gap-5">
+                <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
+                  {proyect.name}
+                </h1>
+                <p className="text-[#8992af] ">{proyect.description}</p>
+                <div className="flex gap-4 text-[#515667] text-sm">
+                  {proyect.tecnologies.map((tecnology) => {
+                    return <p>{tecnology}</p>;
+                  })}
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              Get That Home
-            </h1>
-            <p className="text-[#8992af] ">
-              A Full Stack web application to buy and sell homes{" "}
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>React</p>
-              <p>Emotion</p>
-              <p>Vite</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/pokedex"
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-              <Link
-                href="https://pokedex-beta-pied.vercel.app/"
-                target="_blank"
-              >
-                <PiArrowSquareOutBold fontSize={25} className="changer" />
-              </Link>
-            </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              Pokedex
-            </h1>
-            <p className="text-[#8992af] ">
-              A copy of a pokedex from the pokemon anime{" "}
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>React</p>
-              <p>Emotion</p>
-              <p>React Icons</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/eatable-fillters"
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-            </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              Eatable Fillters
-            </h1>
-            <p className="text-[#8992af] ">
-              An e-comerce food app with login and signup
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>React</p>
-              <p>CSS</p>
-              <p>HTML</p>
-              <p>API fetch</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/Keepable"
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-              <Link href="https://keepable.vercel.app/" target="_blank">
-                <PiArrowSquareOutBold fontSize={25} className="changer" />
-              </Link>
-            </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              Keepable
-            </h1>
-            <p className="text-[#8992af] ">
-              A web application to create notes with all the functionalities{" "}
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>JavaScript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/CLINboards"
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-            </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              CLINboards
-            </h1>
-            <p className="text-[#8992af] ">
-              A Backend App to create as many Boards as you like. Each board can
-              have as many lists and each list as many cards as you want.{" "}
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>Ruby</p>
-              <p>Shell</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="h-[305px] w-[315px] square p-8 lg:hover:-translate-y-2 ease-in duration-150">
-          <div className="flex w-full justify-between items-center ">
-            <CiFolderOn fontSize={50} color="#ff9b2f" />
-            <div className="flex gap-4 items-center">
-              <Link
-                href="https://github.com/NunezGabriel/Clivia_ruby"
-                target="_blank"
-              >
-                <FiGithub fontSize={20} className="changer" />
-              </Link>
-            </div>
-          </div>
-          <div className="grid mt-5 gap-5">
-            <h1 className="text-2xl font-black text-[#cdd6f6] maintext">
-              Clivia Generator
-            </h1>
-            <p className="text-[#8992af] ">
-              A backend application that generates trivia questions consumed
-              from an API{" "}
-            </p>
-            <div className="flex gap-4 text-[#515667] text-sm">
-              <p>Ruby</p>
-              <p>Shell</p>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
       {/*Cuando tengas mas proyectos activa este boton */}
       {/* <button className="w-[200px] h-[50px] border border-[#ff9b2f] rounded-xl changer mx-auto">
---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const proyects = [
+  {
+    name: "Get That Home",
+    linkGitHub: "https://github.com/NunezGabriel/get-that-home-frontend",
+    LinkDeployed: "https://get-that-home-frontend.vercel.app/",
+    description: "A Full Stack web application to buy and sell homes",
+    tecnologies: ["React", "Emotion", "Vite"],
+  },
+  {
+    name: "Pokedex",
+    linkGitHub: "https://github.com/NunezGabriel/pokedex",
+    LinkDeployed: "https://pokedex-beta-pied.vercel.app/",
+    description: "A copy of a pokedex from the pokemon anime",
+    tecnologies: ["React", "Emotion", "PokeAPI"],
+  },
+  {
+    name: "Eatable Fillters",
+    linkGitHub: "https://github.com/NunezGabriel/eatable-fillters",
+    LinkDeployed: "/",
+    description: "An e-comerce food app with login and signup",
+    tecnologies: ["React", "REST API", "Emotion"],
+  },
+  {
+    name: "Keepable",
+    linkGitHub: "https://github.com/NunezGabriel/Keepable",
+    LinkDeployed: "https://keepable.vercel.app/",
+    description:
+      "A web application to create notes with all the functionalities",
+    tecnologies: ["JavaScript", "Html", "Css"],
+  },
+  {
+    name: "CLINboards",
+    linkGitHub: "https://github.com/NunezGabriel/CLINboards",
+    LinkDeployed: "/",
+    description:
+      "A Backend App to create as many Boards as you like. Each board can have as many lists and each list as many cards as you want.",
+    tecnologies: ["Ruby", "Shell"],
+  },
+  {
+    name: "Clivia Generator",
+    linkGitHub: "https://github.com/NunezGabriel/Clivia_ruby",
+    LinkDeployed: "/",
+    description:
+      "A backend application that generates trivia questions consumed from an API",
+    tecnologies: ["Ruby", "Shell"],
+    component: <p>hola</p>,
+  },
+];
